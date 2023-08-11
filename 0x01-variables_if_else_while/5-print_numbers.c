9033aff1b0df50b4e815aea1214883fd214a7b24
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-11 16:25:27 -0700

    Mango ;)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C code prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+
+}
