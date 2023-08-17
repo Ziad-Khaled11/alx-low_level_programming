f435c64ff4fe22cf38f376c945cd67bfc1789abf
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-17 15:25:23 -0700

    Mango ;)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,6 +11,8 @@ void print_times_table(int n)
 {
 int i, j, temp, l;
 
+if (n != 100)
+{
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -31,4 +33,4 @@ printf(",   ");
 putchar('\n');
 }
 }
-
+}
