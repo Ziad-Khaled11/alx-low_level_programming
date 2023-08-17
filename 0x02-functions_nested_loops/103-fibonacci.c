cef6f40d0cdaed3a13404608c4331efe6919bfea
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-17 16:52:21 -0700

    Mango ;)

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C code prints first 50 fibonacci
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int first = 1, second = 2, next, sum;
+	int i;
+
+	sum = 0;
+	for (i = 2; i < 37; i++)
+	{
+		next = first + second;
+		if (next % 2 == 0)
+			sum = sum + next;
+		first = second;
+		second = next;
+	}
+	printf("%ld", sum);
+	putchar('\n');
+
+	return (0);
+}
