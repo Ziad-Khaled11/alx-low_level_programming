aed1c89cd8769c15c63375de52a9f943ab6e600e
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-17 14:31:31 -0700

    Mango ;)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function prints sum of 2 numbers
+ *
+ * @a: parameter that taken to be checked
+ *
+ * @b: parameter that taken to be checked
+ *
+ * Return: sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
