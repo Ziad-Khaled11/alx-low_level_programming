22206648e466fa237b6722a426312f11f366b23f
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-17 14:56:21 -0700

    Mango ;)

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function prints print to 98
+ *
+ * @n: parameter that taken to be checked
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			printf(", ");
+		}
+	}
+	else if (n == 98)
+		printf("%d", 98);
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	putchar(' ');
+}
