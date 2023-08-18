b131ea07ac22650ea1931d90e241a17d689fff88
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-18 10:08:42 -0700

    Mango ;)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function prints multiple of two numbers
+ *
+ * @a: parameter that taken to be checked
+ * @b: parameter that taken to be checked
+ *
+ * Return: multiple of two a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
