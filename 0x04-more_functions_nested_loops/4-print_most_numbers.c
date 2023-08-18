e84ff9fab36f44ae02588c0f800735b2490a0a7c
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-18 10:20:47 -0700

    Mango ;)

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		_putchar(a);
+	}
+	_putchar('\n');
+}
