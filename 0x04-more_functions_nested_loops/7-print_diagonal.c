c3513c8abfd3619eea145154a5d2242eec42b501
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-18 11:09:38 -0700

    Mango ;)

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function prints
+ *
+ * @n: parameter that taken to be checked
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(92);
+	_putchar('\n');
+}
