ee2503a3e8b9f6fd6563bd9ca38dd82f51759fee
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-18 11:16:39 -0700

    Mango ;)

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
+	{
+		
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar(92);
+		_putchar('\n');
+	}
+	if (n == 0)
 	_putchar('\n');
 }
