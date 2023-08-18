4cf55d633676fd59128beb458236e0fb3d94c0c4
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-18 10:54:07 -0700

    Mango ;)

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,21 +8,21 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
 		
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			_putchar(j + '0');
-			else
+			k = j;
+			if (j > 9)
 			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
+				_putchar(49);
+				k = j % 10;
 			}
+			_putchar(k + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
