d5f8b4b855ceaef39c05347d05d09c30680ac6cc
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-21 15:04:14 -0700

    Mango ;)

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - function prints
+ *
+ * @dest: parameter that taken to be checked
+ * @src: parameter that taken to be checked
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = -1;
+
+do {
+itt;
+dest[i] = src[i];
+} while (src[i] != '\0');
+return (dest);
+}
