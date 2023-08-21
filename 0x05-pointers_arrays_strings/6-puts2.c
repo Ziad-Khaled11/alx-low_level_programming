370cfd17fe257c6eaa78a3110ea0ac60556b9e93
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-21 14:42:56 -0700

    Mango ;)

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		_putchar(str[i] + 0);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
