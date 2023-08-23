e8e99875be17d8239ffc7971b9c76ff92ff4b90e
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-23 16:10:16 -0700

    Mango ;)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function prints
+ *
+ * @dest: parameter that taken to be checked
+ * @scr: parameter that taken to be checked
+ * @n: parameter that taken to be checked
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j]; j++)
+	{
+		dest[j] = src[j];
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
+}
