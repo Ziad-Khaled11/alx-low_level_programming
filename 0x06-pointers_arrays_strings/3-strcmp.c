dd4c875ee334d72cef2896dfcb18458279feb85a
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-23 16:26:35 -0700

    Mango ;)

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - function prints
+ *
+ * @s1: parameter that taken to be checked
+ * @s2: parameter that taken to be checked
+ *
+ * Return: comparison result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+}
