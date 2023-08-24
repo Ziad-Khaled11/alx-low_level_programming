a69435a36d2ae5347cc832268df8a832f42717e0
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-23 18:34:26 -0700

    Mango ;)

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *leet - function prints
+ *
+ * @arr: parameter that taken to be checked
+ *
+ * Return: arr
+ */
+
+char *leet(char *arr)
+{
+	char letters[] = {'a', 'e', 'o', 't', 'l'};
+	int nums[] = {4, 3, 0, 7, 1};
+	int i = 0;
+
+	while (arr)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*arr == letters[i] || *arr == letters[i] - 32)
+			       *arr = nums[i] + 48;
+		}
+		c++;
+	}
+	return (c);
+}
