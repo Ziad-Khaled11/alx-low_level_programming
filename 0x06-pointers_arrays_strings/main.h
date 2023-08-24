7e901a2ed468e3349178d6f87978fd62962de175
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-24 16:05:09 -0700

    Mango ;)

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,6 @@ char *rot13(char *arr);
 void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void rev_string(char *s);
+void print_buffer(char *b, int size);
 
 #endif
