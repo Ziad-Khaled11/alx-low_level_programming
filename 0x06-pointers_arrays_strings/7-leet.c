fb0dfa053deb385ea75a16ebd83dee23ae4c4476
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-23 18:41:48 -0700

    Mango ;)

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,18 +10,21 @@
 
 char *leet(char *arr)
 {
+	char *c = arr;
 	char letters[] = {'a', 'e', 'o', 't', 'l'};
 	int nums[] = {4, 3, 0, 7, 1};
-	int i = 0;
+	unsigned int i = 0;
 
-	while (arr)
+	while (*arr)
 	{
-		for (i = 0; i < 5; i++)
+		for (i = 0; i < sizeof(letters) / sizeof(char); i++)
 		{
 			if (*arr == letters[i] || *arr == letters[i] - 32)
-			       *arr = nums[i] + 48;
+			{
+				*arr = nums[i] + 48;
+			}
 		}
 		arr++;
 	}
-	return (arr);
+	return (c);
 }
