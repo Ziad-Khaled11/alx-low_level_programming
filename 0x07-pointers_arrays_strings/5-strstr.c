4363aa7956139a12eaf95da81b5ed1ef1131867e
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-28 12:21:08 -0700

    Mango ;)

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdbool.h>
 
 /**
  * *_strstr - function prints
@@ -11,12 +12,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+int index;
 
-	for (i = 0; haystack[i]; i++)
-	{
-		if (haystack[i] == needle[0])
-			return (haystack + i);
-	}
-	return ('\0');
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+index = 0;
+if (haystack[index] == needle[index])
+{
+do {
+if (needle[index + 1] == '\0')
+return (haystack);
+index++;
+} while (haystack[index] == needle[index]);
+}
+haystack++;
+}
+return ('\0');
 }
