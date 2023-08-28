17dfc6aba897cf548127df18f4a4506c547b19d9
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-28 14:07:51 -0700

    Mango ;)

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function prints
+ *
+ * @s: parameter that taken to be checked
+ * @to: parameter that taken to be checked
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
