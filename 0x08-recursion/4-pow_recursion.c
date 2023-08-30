1bf4d0aa4f60344fc5fe523b6f713b980ce9aaeb
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-30 16:00:34 -0700

    Mango ;)

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function prints power of x
+ *
+ * @x: parameter that taken to be checked
+ * @y: parameter that taken to be checked
+ *
+ * Return: power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0 || x == 1)
+		return (1);
+	return (x * _pow_recursion(x , y - 1));
+
+				
+}
