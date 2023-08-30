6e4277922667c9649a364539e53451ab4ac0c83b
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-08-30 16:44:02 -0700

    Mango ;)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,26 +8,25 @@
  * Return: square root
  */
 
+
 int _sqrt_recursion(int n)
 {
 return (square(n, 1));
 }
-#include "main.h"
 
 /**
- * sq - function prints square
+ * square - function prints square
  *
  * @n: parameter that taken to be checked
  * @val: parameter that taken to be checked
  *
  * Return: square
  */
-int sq(int n, int val)
+int square(int n, int val)
 {
 if (val * val == n)
 return (val);
 else if (val * val < n)
-return (sq(n, val + 1));
-e1se
+return (square(n, val + 1));
 return (-1);
 }
