7501bca566542559b7b515bb14b19b528556805f
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-03 01:25:45 +0300

    \;

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
-*
-* Description: A C code
-*
-* Return: Always 0 (Success)
+ * main - Entry point
+ * @argv:parameter to be executed
+ * @argc:parameter to be executed
+ * Description:A c code prints name of file
+ * Return:Always 0 on success
 */
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
+
