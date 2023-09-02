361772baf959492f8a5391b5fa669aec148b2e04
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-03 00:59:06 +0300

    myteen betty

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A C code
+*
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[]) 
+{
+	printf("%s\n", argv[argc-1]);
+	return (0);
+}
