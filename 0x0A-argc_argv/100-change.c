1398ea6972d025725ce5bcc9a1112995b8e5230e
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-03 03:57:34 +0300

    myteen betty ana haleto

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,117 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-	int num, co = 0;
+if (argc == 2)
+{
+int i, leastcents = 0, money = atoi(argv[1]);
 
-	if (argc == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		num = atoi(argv[1]);
-        if (num < 0)
-		{
-			printf("0\n");
-		}
-		else if (num >= 2 && num < 5)
-		{
-			while (num > 0)
-			{
-				if (num >= 2)
-				{
-					num -= 2;
-					co++;
-				}
-				else
-				{
-					num -= 1;
-					co++;
-				}
+int cents[] = {25, 10, 5, 2, 1};
+for (i = 0; i < 5; i++)
+{
+if (money >= cents[i])
+{
+leastcents += money / cents[i];
+money = money % cents[i];
+if (money % cents[i] == 0)
+{
+break;
+}
+}
+}
+printf("%d\n", leastcents);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
 
-			}
-		}
-		else if (num >= 5 && num < 10)
-		{
-			while (num > 0)
-			{
-				if (num >= 5)
-                {
-                    num -= 5;
-                    co++;
-                }
-				else if (num >= 2)
-				{
-					num -= 2;
-                    co++;
-				}
-                else
-                {
-                    num -= 1;
-                    co++;
-                }
-			}
-		}
-		else if (num >= 10 && num < 25)
-		{
-			while (num > 0)
-            {
-                if (num >= 10)
-                {
-                    num -= 10;
-                    co++;
-                }
-                else if (num >= 5)
-                {
-                    num -= 5;
-                    co++;
-                }
-                else if (num >= 2)
-                {
-                    num -= 2;
-                    co++;
-                }
-                else
-                {
-                    num -= 1;
-                    co++;
-                }
-            }
-		}
-		else if (num >= 25)
-		{
-			while (num > 0)
-            {
-                if (num >= 25)
-                {
-                    num -= 25;
-                    co++;
-                }
-                else if (num >= 10)
-                {
-                    num -= 10;
-                    co++;
-                }
-                else if (num >= 5)
-                {
-                    num -= 5;
-                    co++;
-                }
-                else if (num >= 2)
-                {
-                    num -= 2;
-                    co++;
-                }
-                else
-                {
-                    num -= 1;
-                    co++;
-                }
-            }
-		}
-				
-	}
-	printf("%d\n", co);
-	return (0);
+return (0);
 }
