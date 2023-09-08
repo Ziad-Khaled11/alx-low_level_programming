9b9bb3f1c8e7108be908cf10e921ddfeb8e018c6
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-08 20:39:45 +0300

    ;

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,10 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	
+
 	array = (char *)malloc(sizeof(char) * size);
 	if (size == 0 || array == 0)
 	return ('\0');
-	
 	for (i = 0; i < size; i++)
 	array[i] = c;
 	return (array);
