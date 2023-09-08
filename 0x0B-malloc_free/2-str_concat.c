6a3cc51f780707e2a2d7772463d7cc10c8c46e9e
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-08 21:23:53 +0300

    mango

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2, i;
 	char *arr;
 
+	if (s1 == NULL)
+	s1 = '\0';
+	if (s2 == NULL)
+	s2 = '\0';
 	for (size1 = 0; s1[size1]; size1++)
 	;
 	for (size2 = 0; s2[size2]; size2++)
