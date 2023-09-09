0c29d03f379546d1f50541dceedd29ec71bfbebe
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-09 21:34:23 +0300

    lol

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include "main.h"
 
+#include "main.h"
+
+/**
+ * *_memset - function intialization array by 0
+ *
+ * @s: parameter that taken to be checked
+ * @b: parameter that taken to be checked
+ * @n: parameter that taken to be checked
+ *
+ * Return: array
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ch = s;
+
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (ch);
+}
+
 /**
  * *_calloc - function allocates an array
  *
@@ -14,18 +37,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 	int n;
-	char *ch;
 
 	if (nmemb == 0 || size == 0)
 	return (NULL);
 	ptr = malloc(sizeof(int) * nmemb);
 	if (ptr == 0)
 	return ('\0');
-	n = nmemb * sizeof(int);
-	ch = ptr;
-	while (n--)
-	{
-		*ch++ = '0';
-	}
+	n = sizeof(int) * nmemb;
+	_memset(ptr, 0, n);
 	return (ptr);
 }
