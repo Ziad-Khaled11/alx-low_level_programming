76883640bc09e95381bd7b1c73e0a3ab7ced523b
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-09 23:03:38 +0300

    lol

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -7,7 +8,7 @@
  * @str: parameter that taken to be checked
  */
 
-void puts(char *str)
+void _puts(char *str)
 {
 int i = 0;
 while (str[i])
