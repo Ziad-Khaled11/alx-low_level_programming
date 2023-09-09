94434baead51b5fc8eb56090dbebd27f94be887a
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-09 22:29:06 +0300

    yarab ostor

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,12 +32,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (new_size > old_size)
+	{
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 	return (NULL);
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		*((char *)new_ptr + i) = *((char *)ptr + i);
+	}
+	free(ptr);
 	}
 	return (new_ptr);
 }
