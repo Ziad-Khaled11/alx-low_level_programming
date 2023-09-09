bc37aaa747f418a736bbf9b3908dfb6363072605
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-09 20:30:50 +0300

    lol

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *string_nconcat - function allocates an array of strings
+ *
+ * @s1: parameter that taken to be checked
+ * @s2: parameter that taken to be checked
+ * @n: parameter that taken to be checked
+ *
+ * Return: array of strings
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i, j;
+	unsigned int size1, size2;
+
+	if (s1 == NULL)
+	s1 = '\0';
+	if (s2 == NULL)
+	s2 = '\0';
+	for (size1 = 0; s1[size1]; size1++)
+	;
+	for (size2 = 0; s2[size2]; size2++)
+	;
+	str = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (str == 0)
+	return ('\0');
+	for (i = 0; i < size1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < size2 && j < n; j++)
+	{
+		str[size1 + j] = s2[j];
+	}
+	str[size1 + size2] = '\0';
+	return (str);
+}
