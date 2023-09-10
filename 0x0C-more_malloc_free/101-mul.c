c697ef92cc8032e459980b35a4f6137a0a14abc7
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-10 21:15:44 +0300

    lol

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,66 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+#define ERR_MSG "Error"
 /**
- * _puts - function prints chars
- *
- * @str: parameter that taken to be checked
- */
-
-void _puts(char *str)
-{
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-}
-
-/**
- * _atoi - function prints
+ * is_digit - function prints chars
  *
  * @s: parameter that taken to be checked
  *
- * Return: number
+ * Return: 0 or 1
  */
 
-int _atoi(const char *s)
+int is_digit(char *s)
 {
-int sign = 1;
-unsigned long int resp = 0, firstNum, i;
-for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+int i = 0;
+while (s[i])
 {
-if (s[firstNum] == '-')
-{
-sign *= -1;
+if (s[i] < '0' || s[i] > '9')
+return (0);
+i++;
 }
-}
-for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-{
-resp *= 10;
-resp += (s[i] - 48);
-}
-return (sign *resp);
+return (1);
 }
 
 /**
- * print_int - function prints
+ * _strlen - function prints
  *
- * @n: parameter that taken to be checked
+ * @s: parameter that taken to be checked
+ *
+ * Return: length
  */
 
-void print_int (unsigned long int n)
+int _strlen(char *s)
 {
-unsigned long int divisor = 1, i, resp;
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-for (; divisor >= 1; n %= divisor, divisor /= 10)
+int i = 0;
+while (s[i] != '\0')
 {
-resp = n / divisor;
-_putchar('0' + resp);
+i++;
 }
+return (i);
+}
+
+/**
+ * errors - function prints error message
+ */
+
+void errors(void)
+{
+printf("Error\n");
+exit(98);
 }
 
 /**
@@ -74,17 +61,45 @@ _putchar('0' + resp);
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-(void)argc;
-if (argc != 3)
+char *s1, *s2;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
+result = malloc(sizeof(int) * len);
+if (!result)
+return (1);
+for (i = 0; i <= len1 + len2; i++)
+result[i] = 0;
+for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-_puts("Error");
-_putchar('\n');
-exit(98);
+digit1 = s1[len1] - '0';
+carry = 0;
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+{
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
 }
-
-print_int(_atoi (argv[1]) * _atoi (argv[2]));
+if (carry > 0)
+result[len1 + len2 + 1] += carry;
+}
+for (i = 0; i < len - 1; i++)
+{
+if (result[i])
+a = 1;
+if (a)
+_putchar(result[i] + '0');
+}
+if (!a)
+_putchar('0');
 _putchar('\n');
+free(result);
 return (0);
 }
