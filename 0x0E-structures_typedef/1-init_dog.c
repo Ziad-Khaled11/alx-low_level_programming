f7c49b619fa4dd43f08275baff45974cd3132484
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-11 23:04:58 +0300

    lol

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - function intialize the dog struct
+ *
+ * @d: parameter that taken to be checked
+ * @age: parameter that taken to be checked
+ * @owner: parameter that taken to be checked
+ * @name: parameter that taken to be checked
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
