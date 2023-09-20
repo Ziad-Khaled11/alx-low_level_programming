34c8d4e6d39a3da1533ce6099cab938bc560ad04
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-21 01:00:30 +0300

    ;

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 		if (separator && i < n - 1)
 		{
-			printf("%s ", s);
+			printf("%s", s);
 		}
 	}
 	putchar('\n');
