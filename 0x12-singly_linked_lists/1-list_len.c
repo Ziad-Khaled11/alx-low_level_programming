751c8b7852f18ddbce7d0be16013f16f8e334500
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-09-22 02:10:01 +0300

    lol

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,13 +11,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		n++;
+		num++;
 		h = h->next;
 	}
-	return (n);
+	return (num);
 }
 
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,21 +12,22 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *n;
 	unsigned int len = 0;
 
 	while (str[len])
+	{
 		len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
+	}
+	n = malloc(sizeof(list_t));
+	if (!n)
+	{
 		return (NULL);
-
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-
+	}
+	n->str = strdup(str);
+	n->len = len;
+	n->next = (*head);
+	(*head) = n;
 	return (*head);
 }
 
